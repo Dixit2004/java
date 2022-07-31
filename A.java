e668e4a67af2075a55f338ab6189496590e61e47
Dixit2004 <110329152+Dixit2004@users.noreply.github.com>
2022-07-31 23:01:32 +0530

    java files

---
+++ A.java
@@ -0,0 +1,14 @@
+
+import  java.util.Scanner;
+class A{
+
+public static void main (String args[])
+{
+
+Scanner myobj=new Scanner(System.in); 
+
+System.out.println("Enter a value"); 
+String  a = myobj.nextline(); 
+System.out.println("A value "+a);
+
+}}
---
+++ Ab.java
@@ -0,0 +1,39 @@
+class Ab{
+
+
+
+
+public static void main(String args[]) 
+{
+
+int a=6; 
+
+
+switch (a)
+{ 
+case 1: 
+System.out.print("hello"); 
+break; 
+case 2:
+System.out.print("How are you"); 
+break; 
+case 3:
+System.out.print("Dixit"); 
+break;
+case 4: 
+System.out.print("Dabhi"); 
+case 5: 
+System.out.print("Information technology"); 
+break; 
+default :
+System.out.print("Not valid "); 
+
+
+}
+
+
+}
+
+
+
+}
